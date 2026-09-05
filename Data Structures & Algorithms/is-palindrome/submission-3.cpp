bd656fad46952ed15d24907305cf4f53d21611a1
Kailash Gautham <103820198+kailashgautham@users.noreply.github.com>
2026-09-06 00:02:50 +0800

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0, end = s.size() - 1;
+        while (start < end) {
+            if (!std::isalnum(s[start])) {
+                start++;
+                continue;
+            }
+            if (!std::isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (std::tolower(s[start]) != std::tolower(s[end])) return false;
+            start++, end--;
+        }
+        return true;
+    }
+};
